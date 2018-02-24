9fd27307a472ea20808fded530ff16fd1d233572
admblack97 <admblack97@gmail.com>
2018-02-24 13:20:16 +0700

    perubahan validasi edit data admin

--- resources/views/user/EditDataAdmin.blade.php
+++ resources/views/user/EditDataAdmin.blade.php
@@ -70,7 +70,7 @@
                     Username
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
-                    <input type="text" class="form-control col-md-12 col-xs-12" name="username" value="{{$Admin->User->username}}" required pattern="^[A-Za-z0-9_.]{6,}$" title="Input tidak menggunakan spasi">
+                    <input type="text" class="form-control col-md-12 col-xs-12" name="username" value="{{$Admin->User->username}}" required pattern="^[A-Za-z0-9_.]{6,}$" title="Input tidak menggunakan spasi dan hanya menggunakan simbol _ .">
                   </div>
                 </div>
                 <div class="form-group">
