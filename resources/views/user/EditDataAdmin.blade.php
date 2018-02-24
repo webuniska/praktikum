b278fb35906627943ab96ee0f1c942cf98bb93c8
admblack97 <admblack97@gmail.com>
2018-02-24 13:15:28 +0700

    validasi edit data admin

--- resources/views/user/EditDataAdmin.blade.php
+++ resources/views/user/EditDataAdmin.blade.php
@@ -27,7 +27,7 @@
                     No. Induk
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
-                    <input type="text" class="form-control col-md-12 col-xs-12" name="nomorinduk" value="{{$Admin->nomorinduk}}" required>
+                    <input type="text" class="form-control col-md-12 col-xs-12" name="nomorinduk" value="{{$Admin->nomorinduk}}" required pattern="[0-9]+" title="Input hanya boleh angka dan tidak menggunakan spasi">
                   </div>
                 </div>
                 <div class="form-group">
@@ -35,7 +35,7 @@
                     Nama
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
-                    <input type="text" class="form-control col-md-12 col-xs-12" name="nama" value="{{$Admin->nama}}" required>
+                    <input type="text" class="form-control col-md-12 col-xs-12" name="nama" value="{{$Admin->nama}}" required pattern="(.,[a-z]\s)(.,[A-Z]\s).{6,}+" title="Input tidak menggunakan angka">
                   </div>
                 </div>
                 <div class="form-group">
@@ -43,7 +43,7 @@
                     No. Hp
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
-                    <input type="text" class="form-control col-md-12 col-xs-12" name="nohp" value="{{$Admin->nohp}}" required>
+                    <input type="text" class="form-control col-md-12 col-xs-12" name="nohp" value="{{$Admin->nohp}}" required pattern="[0-9]+" title="Input hanya boleh angka dan tidak menggunakan spasi">
                   </div>
                 </div>
                 <div class="form-group">
@@ -70,7 +70,7 @@
                     Username
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
-                    <input type="text" class="form-control col-md-12 col-xs-12" name="username" value="{{$Admin->User->username}}" required>
+                    <input type="text" class="form-control col-md-12 col-xs-12" name="username" value="{{$Admin->User->username}}" required pattern="^[A-Za-z0-9_.]{6,}$" title="Input tidak menggunakan spasi">
                   </div>
                 </div>
                 <div class="form-group">
