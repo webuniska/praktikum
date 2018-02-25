09bb98e9760194e7c0f66743508617f9f6d43675
admblack97 <admblack97@gmail.com>
2018-02-26 00:02:20 +0700

    validasi tambah data dosen

--- resources/views/user/TambahDataDosen.blade.php
+++ resources/views/user/TambahDataDosen.blade.php
@@ -80,7 +80,7 @@
                     Username
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
-                    <input type="text" class="form-control col-md-12 col-xs-12" name="username" value=" " required pattern="^[A-Za-z0-9]{6,}$" title="Input tidak menggunakan spasi dan dan minimal 6 karakter">
+                    <input type="text" class="form-control col-md-12 col-xs-12" name="username" value=" " required pattern="^[A-Za-z0-9]{6,}$" title="Input tidak menggunakan spasi dan minimal 6 karakter">
                   </div>
                 </div>
                 <div class="form-group">
