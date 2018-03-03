e55fdf147d87ebf43724a461ed6384f276614855
admblack97 <admblack97@gmail.com>
2018-03-03 15:43:12 +0700

    keterangan upload foto

--- resources/views/user/TambahDataAdmin.blade.php
+++ resources/views/user/TambahDataAdmin.blade.php
@@ -60,6 +60,9 @@
                   </label>
                   <div class="col-md-9 col-sm-9 col-xs-12">
                     <input type="file" class="form-control col-md-12 col-xs-12" name="foto">
+                    <label class="control-label text-left col-md-5 col-sm-7 col-xs-12">
+                        Maksimal foto yang di upload berukuran 2 MB.
+                    </label>
                   </div>
                 </div>
                 <hr>
--- resources/views/user/TambahDataMahasiswa.blade.php
+++ resources/views/user/TambahDataMahasiswa.blade.php
@@ -61,6 +61,9 @@
                   <div class="col-md-9 col-sm-9 col-xs-12">
                     <input type="file" class="form-control col-md-12 col-xs-12" name="foto">
                   </div>
+                  <label class="control-label text-left col-md-5 col-sm-7 col-xs-12">
+                      Maksimal foto yang di upload berukuran 2 MB.
+                  </label>
                 </div>
                 <hr>
                 <div class="form-group">
